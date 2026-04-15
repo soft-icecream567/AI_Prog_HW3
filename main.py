82862718f382fdfd26e4922dd7724ce00fe76a01
soft-icecream567 <514861175@qq.com>
2026-04-15 09:24:04 +0800

    step 1.2: 时间解析，格式化时间并给出hour列

--- main.py
+++ main.py
@@ -23,3 +23,14 @@ print(f"数据列数：{raw_data.shape[1]} 列")
 print("\n各列数据类型：")
 print(raw_data.dtypes)
 
+#1.2 时间解析
+#使用 pd.to_datetime 转换，format ,让时间输出格式更加清晰
+raw_data["交易时间"]=pd.to_datatime(raw_data['交易时间'],format='%Y/%m/%d %H:%M')
+
+# 从交易时间中提取小时（整数 0~23），新增为 'hour' 列
+raw_data['hour'] = raw_data['交易时间'].dt.hour
+
+print("打印新增hour列：")
+print(raw_data[['交易时间','hour']].head())#后续时间分析必须依赖hour
+
+
